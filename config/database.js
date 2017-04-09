ae72435e3b796b9ee189e1b0fd608300bf555f79
Musab Afzal <Musab Afzal>
2017-04-09 21:18:20 +0500

    done

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ var state = {
 
 exports.connect = function(done) {
   state.pool = mysql.createPool({
-    host: '192.168.51.100',
+    host: '192.168.103.125',
     user: 'root',
     password: 'lolbro',
     database: 'attendanceDB'
